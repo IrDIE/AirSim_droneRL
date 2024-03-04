3278bb7cc9e345b70eb5366bd21a693e6e56c70e
Дынина Ирина <dynina.irina@stratim.tech>
2024-03-04 18:45:51 +0300

    add file

---
+++ baselines_wrappers/__init__.py
@@ -0,0 +1,7 @@
+from .vec_env import AlreadySteppingError, NotSteppingError, VecEnv, VecEnvWrapper, VecEnvObservationWrapper, CloudpickleWrapper
+from .dummy_vec_env import DummyVecEnv
+from .subproc_vec_env import SubprocVecEnv
+from .monitor import Monitor
+
+__all__ = ['AlreadySteppingError', 'NotSteppingError', 'VecEnv', 'VecEnvWrapper',
+           'VecEnvObservationWrapper', 'CloudpickleWrapper', 'DummyVecEnv', 'SubprocVecEnv']
